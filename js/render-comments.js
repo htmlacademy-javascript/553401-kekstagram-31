f3e4813e4714aea183bed27aea5a87cf5e315b9a
a-contrast <a.contrast@yandex.ru>
2024-03-23 15:20:07 +0300

    добавлена загрузка комментариев

--- js/render-comments.js
+++ js/render-comments.js
@@ -1,19 +1,45 @@
+const AMOUNT_COMMENTS_SHOWN = 5;
+
 const commentList = document.querySelector('.social__comments');
+const commentLoadBtn = document.querySelector('.comments-loader');
+const commentCount = document.querySelector('.social__comment-shown-count');
 const commentListFragment = document.createDocumentFragment();
+const template = commentList.children[0].cloneNode(true);
+
+const createComment = (item) => {
+  const commentItem = template.cloneNode(true);
+
+  commentItem.querySelector('.social__picture').src = item.avatar;
+  commentItem.querySelector('.social__picture').alt = item.name;
+  commentItem.querySelector('.social__text').textContent = item.message;
+
+  return commentItem;
+};
 
 const renderComments = (commentArray) => {
-  commentArray.forEach((comment) => {
-    const commentItem = commentList.children[0].cloneNode(true);
+  let counter = 0;
 
-    commentItem.querySelector('.social__picture').src = comment.avatar;
-    commentItem.querySelector('.social__picture').alt = comment.name;
-    commentItem.querySelector('.social__text').textContent = comment.message;
+  const shownComment = () => {
+    const startIndex = counter * AMOUNT_COMMENTS_SHOWN;
+    const endIndex =
+      startIndex + AMOUNT_COMMENTS_SHOWN > commentArray.length
+        ? commentArray.length
+        : startIndex + AMOUNT_COMMENTS_SHOWN;
 
-    commentListFragment.append(commentItem);
-  });
+    for (let i = startIndex; i < endIndex; i++) {
+      commentListFragment.append(createComment(commentArray[i]));
+    }
+
+    counter++;
+    commentCount.textContent = endIndex;
+    commentLoadBtn.classList.toggle('hidden', endIndex === commentArray.length);
+    commentList.append(commentListFragment);
+  };
 
   commentList.innerHTML = '';
-  commentList.append(commentListFragment);
+  shownComment();
+
+  commentLoadBtn.addEventListener('click', shownComment);
 };
 
 export default renderComments;
--- js/render-full-picture.js
+++ js/render-full-picture.js
@@ -4,13 +4,12 @@ import renderComments from './render-comments.js';
 const body = document.body;
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureCloseBtn = bigPicture.querySelector('.big-picture__cancel');
-const commentCountBlock = bigPicture.querySelector('.social__comment-count');
-const commentLoadBtn = bigPicture.querySelector('.comments-loader');
+// const commentCountBlock = bigPicture.querySelector('.social__comment-count');
+// const commentLoadBtn = bigPicture.querySelector('.comments-loader');
 
 const renderBigPicture = ({ url, description, likes, comments }) => {
   bigPicture.querySelector('img').src = url;
   bigPicture.querySelector('.likes-count').textContent = likes;
-  bigPicture.querySelector('.social__comment-shown-count').textContent = '5'; // заглушка
   bigPicture.querySelector('.social__comment-total-count').textContent =
     comments.length;
   bigPicture.querySelector('.social__caption').textContent = description;
@@ -29,8 +28,8 @@ function openBigPicture(item) {
   renderBigPicture(item);
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
-  commentCountBlock.classList.add('hidden'); // временная заглушка
-  commentLoadBtn.classList.add('hidden'); // временная заглушка
+  // commentCountBlock.classList.add('hidden'); // временная заглушка
+  // commentLoadBtn.classList.add('hidden'); // временная заглушка
   document.addEventListener('keydown', onDocumentKeydown);
   bigPictureCloseBtn.addEventListener('click', closeBigPicture);
 }
@@ -38,8 +37,8 @@ function openBigPicture(item) {
 function closeBigPicture() {
   bigPicture.classList.add('hidden');
   body.classList.remove('modal-open');
-  commentCountBlock.classList.remove('hidden'); // временная заглушка
-  commentLoadBtn.classList.remove('hidden'); // временная заглушка
+  // commentCountBlock.classList.remove('hidden'); // временная заглушка
+  // commentLoadBtn.classList.remove('hidden'); // временная заглушка
   document.removeEventListener('keydown', onDocumentKeydown);
   bigPictureCloseBtn.removeEventListener('click', closeBigPicture);
 }
