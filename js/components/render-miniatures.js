f0bc75cda77e36282b189de1f8c94ccc1db726f3
a-contrast <a.contrast@yandex.ru>
2024-04-07 19:46:45 +0300

    решил проблему дублирования обработчика на pictureList

--- js/components/render-miniatures.js
+++ js/components/render-miniatures.js
@@ -10,36 +10,38 @@ const errorDataTemplate = document
   .content.querySelector('.data-error');
 
 const pictureListFragment = document.createDocumentFragment();
+let filteredArray = [];
 
 const renderMiniatures = (array) => {
-  getFilteredArray(array).forEach(
-    ({ id, url, description, likes, comments }) => {
-      const miniatureItem = templatePicture.cloneNode(true);
-      miniatureItem.dataset.photoId = id;
-      miniatureItem.querySelector('.picture__img').src = url;
-      miniatureItem.querySelector('.picture__img').alt = description;
-      miniatureItem.querySelector('.picture__likes').textContent = likes;
-      miniatureItem.querySelector('.picture__comments').textContent =
-        comments.length;
+  filteredArray = getFilteredArray(array);
+  filteredArray.forEach(({ id, url, description, likes, comments }) => {
+    const miniatureItem = templatePicture.cloneNode(true);
+    miniatureItem.dataset.photoId = id;
+    miniatureItem.querySelector('.picture__img').src = url;
+    miniatureItem.querySelector('.picture__img').alt = description;
+    miniatureItem.querySelector('.picture__likes').textContent = likes;
+    miniatureItem.querySelector('.picture__comments').textContent =
+      comments.length;
 
-      pictureListFragment.append(miniatureItem);
-    }
-  );
+    pictureListFragment.append(miniatureItem);
+  });
 
   pictureList.querySelectorAll('.picture').forEach((node) => node.remove());
   pictureList.append(pictureListFragment);
-
-  pictureList.addEventListener('click', (evt) => {
-    if (evt.target.closest('.picture')) {
-      evt.preventDefault();
-
-      const picture = evt.target.closest('.picture');
-      const index = array.findIndex(
-        (item) => item.id === Number(picture.dataset.photoId)
-      );
-      openBigPicture(array[index]);
-    }
-  });
 };
+
+const onPictureListClick = (evt) => {
+  if (evt.target.closest('.picture')) {
+    evt.preventDefault();
+
+    const picture = evt.target.closest('.picture');
+    const index = filteredArray.findIndex(
+      (item) => item.id === Number(picture.dataset.photoId)
+    );
+    openBigPicture(filteredArray[index]);
+  }
+};
+
+pictureList.addEventListener('click', onPictureListClick);
 
 export { renderMiniatures, errorDataTemplate };
